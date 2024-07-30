592f3d08ff13b336624c2e1826961a8b1f0ba4df
Bryan Schneller <bryan.schneller@gmail.com>
2024-07-30 15:21:23 -0400

    Adjust styling and hierarchy

--- script.js
+++ script.js
@@ -10,15 +10,24 @@ const div7 = document.getElementById('7');
 const div8 = document.getElementById('8');
 const div9 = document.getElementById('9');
 
+const play =  document.getElementById('play');
+
 const divArray = [div1, div2, div3, div4, div5, div6, div7, div8, div9];
 
-for(let i = 0; i < divArray.length; i++){
-    let colors = ['black', 'red', 'blue', 'yellow'];
-    let random = Math.floor(Math.random()*4);
-    divArray[i].style.backgroundColor = `${colors[random]}`;
-    let random2 = Math.floor(Math.random()*4);
-    divArray[i].style.border = `25px solid ${colors[random2]}`;
-    if(random === random2){
-        divArray[i].textContent = 'Same!'
+const runGame = () => {
+    
+        for(let i = 0; i < divArray.length; i++){
+            divArray[i].textContent = '';
+        let colors = ['black', 'red', 'blue', 'yellow'];
+        let random = Math.floor(Math.random()*4);
+        divArray[i].style.backgroundColor = `${colors[random]}`;
+        let random2 = Math.floor(Math.random()*4);
+        divArray[i].style.border = `25px solid ${colors[random2]}`;
+        if(random === random2){
+            divArray[i].textContent = 'Same!'
+        }
     }
 }
+
+play.addEventListener('click', runGame);
+
